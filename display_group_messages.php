6461ad2572c7c7eb988d5ce0c4ad161a8fd97bb3
tanvirt <tanvirt@ufl.edu>
2014-04-20 02:36:55 -0400

    added group message viewing

--- display_group_messages.php
+++ display_group_messages.php
@@ -6,6 +6,51 @@ $dbh = new PDO('pgsql:host='.$db->{'host'}.';port='.$db->{'port'}.';dbname='.$db
 
 $group_name = $_GET['group_name'];	
 
-echo 'Group Message!';
+$query = "SELECT username, message, 
+				TO_CHAR(time_posted, 'Mon dd') AS date, EXTRACT(HOUR FROM time_posted) AS hour, EXTRACT(MINUTE FROM time_posted) AS minute
+			FROM group_messages
+			WHERE group_name = :group_name
+			ORDER BY id DESC";
+$stmt = $dbh->prepare($query);
+$stmt->bindParam(':group_name', $group_name);
+$stmt->execute();
+
+$message = $stmt->fetch();
+
+function display_message($message) {
+	$minute = '0';
+	$hour = '';
+	if($message['minute'] < 10)
+		$minute .= $message['minute'];
+	else
+		$minute = ''.$message['minute'];
+	if($message['hour'] > 12) {
+		$hour .= ($message['hour'] - 12);
+		$minute .= 'pm';
+	}
+	else {
+		$hour .= $message['hour'];
+		$minute .= 'am';
+	}
+	echo '<div class="message-box">';
+	echo '<button class="button8" type="button" disabled>' 
+			. $message['username'] . ' | ' 
+			. $message['date'] . ' | '
+			. $hour . ':'
+			. $minute
+			. '</button>';
+	echo '<br/><br/>'.$message['message'].'<br/><br/>';
+	echo '</div>';
+}
+
+if($message) {
+	display_message($message);
+	
+	while($message = $stmt->fetch()) {
+		display_message($message);
+	}
+}
+else
+	echo 'No messages';
 
 $dbh = null;
