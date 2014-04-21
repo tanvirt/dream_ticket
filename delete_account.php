2027b0cea49a1c77da414ced473f116158eec213
tanvirt <tanvirt@ufl.edu>
2014-04-20 22:46:35 -0400

    allowed account deletion

---
+++ delete_account.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Dream Ticket | Account Deleted</title>
+		<link rel="stylesheet" type="text/css" href="login_signup.css"/>
+	</head>
+	<body>
+		<?php
+
+		session_start();
+
+		if(!isset($_SESSION['username']))
+			die('You must be logged in to access this page<br/>'."<a href='login.html'>Click here</a> to log in");
+
+		$db = json_decode(file_get_contents("../db.json"));
+		$dbh = new PDO('pgsql:host='.$db->{'host'}.';port='.$db->{'port'}.';dbname='.$db->{'dbname'}.';user='.$db->{'user'}.';password='.$db->{'password'})
+			or die('Could not connect to database');
+
+		$username = $_SESSION['username'];
+
+		$query = 'DELETE FROM accounts
+					WHERE username = :username';
+		$stmt = $dbh->prepare($query);
+		$stmt->bindParam(':username', $username);
+		$stmt->execute();
+		
+		session_destroy();
+		
+		echo "Your account has been deleted. <a href='login.html'>Click here</a> to return to the login page";
+		
+		$dbh = null;
+		
+		?>
+	</body>
+<html>
--- logout.php
+++ logout.php
@@ -1,7 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Dream Ticket | Logged Out</title>
+		<link rel="stylesheet" type="text/css" href="login_signup.css"/>
+	</head>
+	<body>
+		<?php
 
-session_start();
+		session_start();
 
-session_destroy();
+		session_destroy();
+		
+		echo "You have been logged out. <a href='login.html'>Click here</a> to log back in";
+		
+		?>
+	</body>
+<html>
 
-echo "You have been logged out. <a href='login.html'>Click here</a> to log back in";
